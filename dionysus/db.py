d772ee04509150fe7df5dce8774b8587244adee0
Hemingway <ghostblac@protonmail.com>
2021-07-30 13:43:35 -0400

    Update db.py

--- dionysus/db.py
+++ dionysus/db.py
@@ -5,6 +5,22 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    # Clear the existing data and create new tables.
+
+    init_db()
+    click.echo("Initialized the database.")
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
+
+
 def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
