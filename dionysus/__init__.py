56780794eb0b3ac09996ccc9394c4e5c3a0ddf5b
Hemingway <ghostblac@protonmail.com>
2021-08-03 21:46:12 -0400

    Update __init__.py

--- dionysus/__init__.py
+++ dionysus/__init__.py
@@ -27,7 +27,7 @@ def create_app(test_config=None):
     # a simple page displaying the version
     @app.route("/version")
     def hello():
-        return "Dionysus (Dion v0.0.2-45), online."
+        return "Dionysus (Dion v0.0.3-43), online."
 
     from . import db
     db.init_app(app)
