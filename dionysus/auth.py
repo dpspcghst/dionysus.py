2641b36e44a35eac5837ecb80f57d5027a441937
Hemingway <ghostblac@protonmail.com>
2021-08-03 21:46:39 -0400

    Update auth.py

--- dionysus/auth.py
+++ dionysus/auth.py
@@ -3,13 +3,13 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from flaskr.db import get_db
+from dionysus.db import get_db
 from werkzeug.security import check_password_hash, generate_password_hash
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-@bp.route("/register", methods=("get", "post"))
+@bp.route("/register", methods=("GET", "POST"))
 def register():
     if request.method == "post":
         username = request.form["username"]
