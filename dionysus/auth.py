75da41f20fe636e15fbf1f8015c0d6016cfee139
Hemingway <ghostblac@protonmail.com>
2021-08-09 15:33:30 -0400

    Update auth.py

--- dionysus/auth.py
+++ dionysus/auth.py
@@ -47,6 +47,17 @@ def login():
     return render_template('auth/login.html')
 
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
 @bp.route('/logout')
 def logout():
     session.clear()
