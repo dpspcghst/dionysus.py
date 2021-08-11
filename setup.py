4678bace7510929590c8ea02abf059e460a56ca2
Hemingway <ghostblac@protonmail.com>
2021-08-11 14:30:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='dionysus',
-    version='0.1.2-70',
+    version='0.1.4-22',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
